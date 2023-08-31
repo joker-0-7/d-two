a56b1832f3b8f53af87fb27e7ed2e3f2ac9c8e1f
joker-0-7 <jokere136@gmail.com>
2023-09-01 02:28:59 +0300

    bb

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 async function page({params}) {
-
-// const product = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json())
+const id = params.id
+const product = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`).then((res) => res.json())
 return (
     <div>
       <h1>
         {params.id} dynamic page 
       </h1>
-      {/* <h1>{product ? product.title: ''}</h1> */}
+      <h1>{product ? product.title: 'sorry '}</h1>
     </div>
   )
 }
